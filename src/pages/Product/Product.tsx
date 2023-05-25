200fe7913e5fbab6ed579039d236dfe3af9551b7
tatojajo <tatojajanidze@gmail.com>
2023-05-26 01:13:04 +0200

    newUpdate

--- src/pages/Product/Product.tsx
+++ src/pages/Product/Product.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-<<<<<<< HEAD
 import { useAppSelector, useAppDispatch } from "../../redux/hooks";
 import { useTranslation } from "react-i18next";
 import "react-gallery-carousel/dist/index.css";
@@ -17,30 +16,11 @@ import {
   Image,
   ProductDescription,
   ProductBtns,
-=======
-import { useAppSelector } from "../../redux/hooks";
-import { useTranslation } from "react-i18next";
-import "react-gallery-carousel/dist/index.css";
-import { HomeState } from "../../@types/general";
-import { Box, Typography, IconButton, Paper } from "@mui/material";
-import { ArrowBack, ArrowForward } from "@mui/icons-material";
-import {
-  ProductConatiner,
-  ProductInfoWrapper,
-  ProductImagesWraper,
-  SliderImagesContainer,
-  SliderImageWraper,
-  Image,
-  ProductDescription,
->>>>>>> d69ccb6885efc973fdebca7708c6dbc83dc84c2b
 } from "./ProductPageStyle";
 
 const Product = () => {
   const { t } = useTranslation();
-<<<<<<< HEAD
   const dispatch = useAppDispatch();
-=======
->>>>>>> d69ccb6885efc973fdebca7708c6dbc83dc84c2b
   const [productImage, setProductImage] = useState(0);
   const { selectedProduct } = useAppSelector<HomeState>((state) => state.homeReducer);
 
@@ -61,7 +41,6 @@ const Product = () => {
   };
 
   return (
-<<<<<<< HEAD
     <Box>
       <ProductInfoWrapper>
         <ProductImagesWraper>
@@ -82,23 +61,10 @@ const Product = () => {
                 );
               })}
             </SliderImageWraper>
-=======
-    <Paper sx={{
-      width:'80%',
-      margin:'10px auto',
-      display:'flex',
-      alignItems:'center',
-      justifyContent:'center'
-    }}>
-      <ProductConatiner>
-        <ProductInfoWrapper>
-          <ProductImagesWraper>
->>>>>>> d69ccb6885efc973fdebca7708c6dbc83dc84c2b
             <Box>
               <img
                 src={selectedProduct?.images[productImage]}
                 alt=""
-<<<<<<< HEAD
                 style={{
                   width: "500px",
                   height: "500px",
@@ -144,62 +110,6 @@ const Product = () => {
         </ProductDescription>
       </ProductInfoWrapper>
     </Box>
-=======
-                style={{ backgroundColor: "#D8BFD8	" }}
-              />
-            </Box>
-
-            <SliderImagesContainer maxWidth="sm">
-              {/* <IconButton
-              aria-label=""
-              onClick={prevImage}
-              sx={{ height: "40px", width: "40px" }}
-            >
-              <ArrowBack />
-            </IconButton> */}
-              <SliderImageWraper>
-                {selectedProduct?.images.map((image, index) => {
-                  return (
-                    index < 4 && (
-                      <Image
-                        key={index}
-                        src={image}
-                        index={index}
-                        productImage={productImage}
-                        onClick={() => setProductImage(index)}
-                        alt="image"
-                      />
-                    )
-                  );
-                })}
-              </SliderImageWraper>
-              {/* <IconButton
-              aria-label=""
-              onClick={nextImage}
-              sx={{ height: "40px", width: "40px" }}
-            >
-              <ArrowForward />
-            </IconButton> */}
-            </SliderImagesContainer>
-          </ProductImagesWraper>
-          <ProductDescription>
-            <Typography variant="h3" color="initial">
-              {t("global.brand")}: {selectedProduct?.brand}
-            </Typography>
-            <Typography variant="h5" color="initial">
-              {t("global.model")}: {selectedProduct?.title}
-            </Typography>
-            <Typography variant="h6" color="initial">
-              {t("global.category")}: {selectedProduct?.category}
-            </Typography>
-            <Typography variant="h6" color="initial">
-              {t("global.price")}: ${selectedProduct?.price}
-            </Typography>
-          </ProductDescription>
-        </ProductInfoWrapper>
-      </ProductConatiner>
-    </Paper>
->>>>>>> d69ccb6885efc973fdebca7708c6dbc83dc84c2b
   );
 };
 
--- src/pages/Product/ProductPageStyle.ts
+++ src/pages/Product/ProductPageStyle.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 type ImageProps = {
   index: number;
   productImage: number;
@@ -8,11 +7,6 @@ import { styled, Box, Container } from "@mui/system";
 export const ProductConatiner = styled(Box)`
   background-color: grey;
   margin: 20px auto;
-=======
-import { styled, Box, Container } from "@mui/system";
-
-export const ProductConatiner = styled(Box)`
->>>>>>> d69ccb6885efc973fdebca7708c6dbc83dc84c2b
   display: flex;
   flexdirection: column;
   alignitems: center;
@@ -20,7 +14,6 @@ export const ProductConatiner = styled(Box)`
 `;
 
 export const ProductInfoWrapper = styled(Box)`
-<<<<<<< HEAD
   width: 100%;
   height: 550px;
   display: flex;
@@ -31,40 +24,18 @@ export const ProductInfoWrapper = styled(Box)`
 export const ProductImagesWraper = styled(Box)`
   width: 60rem;
   display: flex;
-=======
-  height: 500px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 30px;
-  margin-top: 60px;
-`;
-
-export const ProductImagesWraper = styled(Box)`
-  width: 50%;
-  height: 300px;
-  display: flex;
-  flex-direction: column;
->>>>>>> d69ccb6885efc973fdebca7708c6dbc83dc84c2b
   align-items: center;
   justify-content: center;
 `;
 
-<<<<<<< HEAD
 export const ProductImagesContainer = styled(Container)`
   display: flex;
   gap: 25px;
-=======
-export const SliderImagesContainer = styled(Container)`
-  display: flex;
-  gap: 10px;
->>>>>>> d69ccb6885efc973fdebca7708c6dbc83dc84c2b
   align-items: center;
 `;
 
 export const SliderImageWraper = styled(Box)`
   display: flex;
-<<<<<<< HEAD
   flex-direction: column;
   gap: 10px;
   align-items: center;
@@ -91,28 +62,5 @@ export const ProductBtns = styled(Box)`
   width: 400px;
   display: flex;
   align-items: center;
-=======
-  gap: 10px;
-  align-items: center;
-  justify-content: center;
-  //   overflow-x: scroll;
-  //   scroll-behavior: smooth;
-`;
-
-export const Image = styled("img")(({ index, productImage }) => ({
-  border: index === productImage ? "2px solid black" : "none",
-  borderRadius: "10px",
-  padding: "10px",
-  width: "100px",
-  height: "100px",
-  backgroundColor: "#D8BFD8	",
-}));
-
-export const ProductDescription = styled(Box)`
-  width: 50%;
-  height: 300px;
-  display: flex;
-  flex-direction: column;
->>>>>>> d69ccb6885efc973fdebca7708c6dbc83dc84c2b
   justify-content: space-between;
 `;
